783f136d12d738a1758ed216691ca2ce17381d44
Stefan Chrisostomus <stefanchrisostomus13@gmail.com>
2020-11-16 17:59:13 +0100

    Added zsh.sh

--- .zshrc
+++ .zshrc
@@ -138,3 +138,4 @@ source ~/powerlevel10k/powerlevel10k.zsh-theme
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 export NNN_TRASH=1    # trash (needs trash-cli) instead of delete
+
---
+++ zsh.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+###############################################################################
+# This script is to be used on 64 bit computers                               #
+# Author: Stefanski1977                                                       #
+# Date: 11-03-2020                                                            #
+# This script will: Install zsh oh-my-zsh and classyTouch theme               #
+###############################################################################
+#                                                                             #
+# Examine before running this script. Run at your own risk.                   #
+#                                                                             #
+###############################################################################
+
+# Install zsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+# Install classyTouch theme
+git clone https://github.com/yarisgutierrez/classyTouch_oh-my-zsh.git 
+
+# Changing directory
+cd classyTouch_oh-my-zsh
+
+# Copy theme to theme folder
+cp classyTouch.zsh-theme ~/.oh-my-zsh/themes
+
+# Changing the theme to classyTouch
+sudo sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"classyTouch\"/g' ~/.zshrc
+
+# Changing shell to zshrc
+chsh -s /bin/zsh stefan
+
+
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
+echo
+echo "Log off or reboot to see the change in your terminal".
+echo "----------------------------------------------------"
+
+
+echo "################################################################"
+echo "###################    zsh installed     #######################"
+echo "################################################################"
